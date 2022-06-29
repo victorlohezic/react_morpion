1e015b39a30149fa8f29467291288d8551ac6288
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2022-06-29 15:15:45 +0200

    Highlight the three squares that caused a win

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Then, I need to complete the following achievements :
 - Bold the currently selected item in the move list. ✅
 - Rewrite Board to use two loops to make the squares instead of hardcoding them. ✅
 - Add a toggle button that lets you sort the moves in either ascending or descending order. ✅
-- When someone wins, highlight the three squares that caused the win. ❌
+- When someone wins, highlight the three squares that caused the win. ✅
 - When no one wins, display a message about the result being a draw ❌
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
+        <button className={"square"} style={props.isWinner ? { backgroundColor: '#1b9e3e', color: 'white' } : { backgroundColor: 'white' }} onClick={props.onClick}>
+                {props.value}
+                {console.log("" + props.isWinner)}
         </button>
     );
 }
 
 class Board extends React.Component {
     renderSquare(i) {
+        // if (this.props.line_winner) {
+        //     console.log(this.props.line_winner.indexOf(i)>=0);
+        // }
         return (
             <Square
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
+                isWinner={this.props.line_winner && this.props.line_winner.indexOf(i) >= 0}
             />
         );
     }
@@ -27,7 +32,7 @@ class Board extends React.Component {
             for (let j = 0; j < 3; ++j) {
                 col.push(this.renderSquare(j + 3 * i));
             }
-            row.push(<div key={"board-"+i} className="board-row">{col}</div>);
+            row.push(<div key={"board-" + i} className="board-row">{col}</div>);
         }
         return (
             <div>{row}</div>
@@ -36,7 +41,7 @@ class Board extends React.Component {
 }
 
 class ToggleButton extends React.Component {
-    
+
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
@@ -101,9 +106,11 @@ class Game extends React.Component {
     render() {
         let history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        const winner = calculateWinner(current.squares);
+        const resultGame = calculateWinner(current.squares);
+        const winner = resultGame ? resultGame[0] : null;
+        const line_winner = resultGame ? resultGame[1] : null;
         const moves = history.map((step, move) => {
-            move = this.state.isAscending ? move : history.length-1 - move;
+            move = this.state.isAscending ? move : history.length - 1 - move;
             const desc = move ?
                 'Revenir au tour n°' + move :
                 'Revenir au début de la partie';
@@ -127,10 +134,11 @@ class Game extends React.Component {
                     <Board
                         squares={current.squares}
                         onClick={(i) => this.handleClick(i)}
+                        line_winner={line_winner}
                     />
                 </div>
                 <div className="game-info">
-                    <ToggleButton 
+                    <ToggleButton
                         toggleClick={() => this.toggleClick()}
                     />
                     <div>{status}</div>
@@ -160,14 +168,14 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return [squares[a], lines[i]];
         }
     }
     return null;
 }
 
 function getX(i, y) {
-    return Math.round((i-y)/3)+1;
+    return Math.round((i - y) / 3) + 1;
 }
 
 function getY(i) {
